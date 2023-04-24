d1aee2ceb71ea2e7028ca495680af82d9941d0c8
SivaBhargaviPesarlanka <123106688+SivaBhargaviPesarlanka@users.noreply.github.com>
2023-04-24 13:53:04 -0500

    Second commit

--- models/animal.js
+++ models/animal.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const animal = mongoose.Schema({
-animal_Name: String,
-animal_breed: String,
-animal_cost: Number
+animal_Name:{ type: String, minlength:1, maxlength:20},
+animal_breed:{type: String, minlength:1, maxlength:20},
+animal_cost: {type: Number, min:100, maxlength:1000}
 })
 module.exports = mongoose.model("Animal",
 animal)
